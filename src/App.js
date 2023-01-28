90733937fd02b353f21b84972ebd84c2f7b3d9b8
KarthikVajja <karthik31032000@gmail.com>
2023-01-28 16:28:22 -0500

    Query Client Provider

--- src/App.js
+++ src/App.js
@@ -5,22 +5,30 @@ import { Contact } from "./pages/Contact";
 import { Profile } from "./pages/Profile";
 import { Navbar } from "./pages/Navbar"
 import { useState, createContext } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const AppContext = createContext();
 
 function App() {
   const [username, setUsername] = useState("Karthik Vajja");
+  const client = new QueryClient({defaultOptions:{
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }});
   return (
     <div className="App">
       <AppContext.Provider value={{username, setUsername}}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/profile" element={<Profile/>} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </Router>
+        <QueryClientProvider client={client}>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/profile" element={<Profile/>} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </Router>
+        </QueryClientProvider>
       </AppContext.Provider>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,25 @@
 import { useContext } from "react";
 import { AppContext } from "../App";
+import { useQuery } from "@tanstack/react-query";
+import Axios from "axios";
+
 export const Home = () => {
+    const { data:catData, isLoading, isError, refetch } = useQuery(["cat"], () => {
+        return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+    });
     const {username} = useContext(AppContext);
+    if (isError){
+        return <h1> There is some error.... </h1>
+    }
+    if (isLoading){
+        return <h1> Loading..... </h1>
+    }
     return (
-        <h1>This is the home page and user is {username} </h1>
+        <div>
+            <h1>This is the home page and user is {username} </h1>
+            <br />
+            <h1> {catData?.fact} </h1>
+            <button onClick={refetch}> Update Data  </button>
+        </div>
     );
 }
