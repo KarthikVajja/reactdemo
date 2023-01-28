ed1ccdb9b00f1700c363d0f113414e356f8b9758
KarthikVajja <karthik31032000@gmail.com>
2023-01-28 15:34:48 -0500

    routing and navbar links

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { Menu } from "./pages/Menu";
 
 function App() {
-  
-  const [name, setName] = useState("");
-  const [predictedAge, setPredictedAge] = useState(0);
-
-  const fetchData = () => {
-    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
-      setPredictedAge(res.data.age);
-    });
-  };
 
   return (
     <div className="App">
-      <input placeholder="Ex. Karthik..." onChange={(event) => {setName(event.target.value)}}/>
-      <button onClick={fetchData}>
-        Predict Age
-      </button>
-
-      <h1> Predicted Age: {predictedAge}</h1>
+      <Router>
+        <div>
+          <Link to="/"> Home </Link>
+          <Link to="/menu"> Menu </Link>
+          <Link to="/contact"> Contact </Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Menu.js
@@ -0,0 +1,5 @@
+export const Menu = () => {
+    return (
+        <h1>This is menu page</h1>
+    );
+}
