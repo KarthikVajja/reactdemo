6b57481c05d3284e5522e312a2c18635d44b6088
KarthikVajja <karthik31032000@gmail.com>
2023-01-28 20:46:06 -0500

    cutomHook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
-import { Form } from "./components/Form";
-
+import { useToggle } from "./components/useToggle";
 function App() {
+  const {state: isVisible, toggle: toggleIsVisible} = useToggle();
   return (
     <div className="App">
-      <Form />
+      <button onClick={toggleIsVisible}>
+        {isVisible ? "Hide" : "Show"}
+      </button>
+      {isVisible && <h1> Hidden Text </h1>}
     </div>
   );
 }
